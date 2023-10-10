62976db7de473ffd9df5a07ddccab2e83a286e61
Desmond Asiedu Asamoah <walterobrien_123@yahoo.com>
2023-10-10 19:28:11 +0100

    Added a new file to the directory 0x16-api_advanced
    Added the file 1-top_ten.py for the task 1 of the project Advanced
    API.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+1-top_ten module
+'''
+
+import sys
+import requests
+
+
+def top_ten(subreddit):
+    '''
+    queries the Reddit API and prints the titles of
+    the first 10 hot posts listed for a given subreddit.
+    '''
+
+    url = 'https://reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {'User-Agent': '{}:0x16.api.advanced:v1 (by u/waltob123)'.format
+               (sys.platform)}
+    params = {'limit': 10}
+
+    response = requests.get(url=url, headers=headers, params=params)
+
+    if response.status_code == 404:
+        print('None')
+        return None
+    results = response.json().get('data')
+    [print(result.get('data').get('title')) for result in
+     results.get('children')]
